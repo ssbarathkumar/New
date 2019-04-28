6f17be56ec416b50f2b5440792f6ec165f3952a9
ssbarathkumar05 <ssbarathkumar05@gmail.com>
2019-04-29 00:04:44 +0530

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "DC!!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name="Customer 1"},
+                new Customer { Name="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers=customers
+            };
+            return View(viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("Id=" + id);
+
+        }
+        
+        /*public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if(string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(string.Format("PageIndex: {0} & sortBy: {1}", pageIndex, sortBy));
+        }*/
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year,int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ Vidly/Models/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Customer> post { get; set; }
+        
+    }
+}
